80ec297d7842a956cef329ab17b6ee235d7e25ca
Oliver Ridsdale <oliver@natural-apptitude.co.uk>
2018-08-04 15:31:48 +0100

    scss updates, global home styles added

--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -3,26 +3,20 @@ import { Component } from '@stencil/core';
 
 @Component({
   tag: 'app-home',
-  styleUrl: 'app-home.css'
+  styleUrl: 'app-home.scss'
 })
 export class AppHome {
 
   render() {
     return (
-      <div class='app-home'>
+      <section class="page">
         <p>
           Welcome to the Stencil App Starter.
           You can use this starter to build entire apps all with
           web components using Stencil!
           Check out our docs on <a href='https://stenciljs.com'>stenciljs.com</a> to get started.
         </p>
-
-        <stencil-route-link url='/profile/stencil'>
-          <button>
-            Profile page
-          </button>
-        </stencil-route-link>
-      </div>
+      </section>
     );
   }
 }
